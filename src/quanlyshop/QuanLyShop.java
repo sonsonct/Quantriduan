1f9b515b7412b3be76f520e582c1b6f338680546
phung thai son <sondzvodoi@gmail.con>
2023-05-10 11:26:21 +0700

    fix loi

---
+++ src/quanlyshop/QuanLyShop.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package quanlyshop;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class QuanLyShop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        DangNhap dn = new DangNhap();
+        dn.setLocationRelativeTo(null);
+        dn.setVisible(true);
+//            QuanLyKhachHang kh = new QuanLyKhachHang();
+//            kh.setLocationRelativeTo(null);
+//            kh.setVisible(true);
+//        QuanLyNhanVien kh = new QuanLyNhanVien();
+//            kh.setLocationRelativeTo(null);
+//            kh.setVisible(true);
+    }
+    
+}
